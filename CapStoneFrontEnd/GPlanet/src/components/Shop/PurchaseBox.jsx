cb09c9970eeb35ad9973be9da67cac7d26960dde
Domy15 <domenico2016dellaventura@gmail.com>
2025-04-18 10:33:40 +0200

    terminato completamente il bottone di acquisto e aggiunta al carrello nella pagina details

--- CapStoneFrontEnd/GPlanet/src/components/Shop/PurchaseBox.jsx
+++ CapStoneFrontEnd/GPlanet/src/components/Shop/PurchaseBox.jsx
@@ -7,7 +7,8 @@ const PurchaseBox = ({ game }) => {
     const update = useSelector(state => state.update);
     const userName = useSelector(state => state.profile.userName);
     const dispatch = useDispatch();
-    const [isThere, setIsThere] = useState(false);
+    const [isThereCart, setIsThereCart] = useState(false);
+    const [isThereLibrary, setIsThereLibrary] = useState(false);
 
     const getCart = async () => {
         const getToken = JSON.parse(localStorage.getItem("token"));
@@ -20,7 +21,7 @@ const PurchaseBox = ({ game }) => {
             });
             if (response.ok) {
                 const data = await response.json();
-                setIsThere(data.cart.some(g => g.id === game.id));
+                setIsThereCart(data.cart.some(g => g.id === game.id));
             } else {
                 throw new Error("Errore nel recupero dei dati!");
             }
@@ -52,33 +53,84 @@ const PurchaseBox = ({ game }) => {
         }
     }
 
+    const getLibrary = async () => {
+        const getToken = JSON.parse(localStorage.getItem("token"));
+        try {
+            const response = await fetch(`https://localhost:7227/api/Library/${userName}`, {
+                method: "GET",
+                headers: {
+                    "Authorization": `Bearer ${getToken.token}`,
+                }
+            });
+            if (response.ok) {
+                const data = await response.json();
+                setIsThereLibrary(data.library.some(g => g.id === game.id));
+            } else {
+                throw new Error("Errore nel recupero dei dati!");
+            }
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    const AddToLibrary = async () => {
+        const getToken = JSON.parse(localStorage.getItem("token"));
+        try {
+            const response = await fetch(`https://localhost:7227/api/Library/${userName}`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${getToken.token}`
+                },
+                body: JSON.stringify([game.id])
+            });
+            if (response.ok) {
+                dispatch({
+                    type: "UPDATE",
+                });
+            } else {
+                throw new Error("Errore nell'aggiunta al carrello!")
+            }
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
     useEffect(() => {
-        getCart();
+        if (userName) {
+            getCart();
+            getLibrary();
+        }
     }, [update])
     return (
-        <>
-            {!isThere ?
-                <div className='d-flex justify-content-between align-items-center mt-5 p-4' style={{ backgroundColor: "#4C5B69" }}>
-                    <h3 className='text-white'>{game.title}</h3>
+        <div className='d-flex justify-content-between align-items-center mt-5 p-4' style={{ backgroundColor: "#4C5B69" }}>
+            <h3 className='text-white'>{game.title}</h3>
 
-                    <div className='bg-black d-flex align-items-center p-1 ps-2' style={{ gap: "8px" }}>
-                        <p className='text-white m-0'>{game.price > 0 ? `${game.price}€` : "Free-to-Play"}</p>
-                        <Button variant={"success"} onClick={addToCart}>
-                            Aggiungi al carrello
-                        </Button>
-                    </div>
-                </div> :
-                <div className='d-flex justify-content-between align-items-center mt-5 p-4' style={{ backgroundColor: "#4C5B69" }}>
-                    <h3 className='text-white'>{game.title}</h3>
+            <div className='bg-black d-flex align-items-center p-1 ps-2' style={{ gap: "8px" }}>
+                <p className='text-white m-0'>{game.price > 0 ? `${game.price}€` : "Free-to-Play"}</p>
 
-                    <div className='bg-black d-flex align-items-center p-1 ps-2' style={{ gap: "8px" }}>
-                        <p className='text-white m-0'>{game.price > 0 ? `${game.price}€` : "Free-to-Play"}</p>
-                        <Button variant={"success"} disabled>
-                            Nel Carrello
-                        </Button>
-                    </div>
-                </div>}
-        </>
+                {game.price === 0 && !isThereLibrary ? (
+                    <Button variant={"success"} onClick={AddToLibrary}>
+                        Aggiungi alla libreria
+                    </Button>
+                ) : isThereLibrary ? (
+                    <Button variant={"success"} disabled>
+                        Presente in Libreria
+                    </Button>
+                ) : isThereCart ? (
+                    <Button variant={"success"} disabled>
+                        Nel Carrello
+                    </Button>
+                ) : (
+                    <Button variant={"success"} onClick={addToCart}>
+                        Aggiungi al carrello
+                    </Button>
+                )}
+
+            </div>
+        </div>
     );
 };
 
